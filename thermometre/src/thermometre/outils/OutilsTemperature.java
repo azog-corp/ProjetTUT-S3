f85b724c7ea035d57b7d31d1398199d3a3537272
Maël Le Folgoc Pontis <maellefolgocpontis@yahoo.fr>
2019-11-06 14:07:20 +0100

    création projet

---
+++ thermometre/src/thermometre/outils/OutilsTemperature.java
@@ -0,0 +1,19 @@
+package thermometre.outils;
+
+import java.util.ArrayList;
+import thermometre.outils.Temperature;
+
+public class OutilsTemperature {
+	
+	private static ArrayList<Temperature> listeTemp = new ArrayList<Temperature>();
+	
+	private static void initTemp() {
+		
+	}
+
+	public static void main(String[] args) {
+		initTemp();
+
+	}
+
+}
---
+++ thermometre/src/thermometre/outils/Temperature.java
@@ -0,0 +1,31 @@
+package thermometre.outils;
+
+import java.util.regex.Pattern;;
+
+public class Temperature {
+	
+	private String date;
+	
+	private String heure;
+	
+	private String temp;
+	
+	/* private static Pattern format = */
+	
+	public Temperature(String temp) {
+		
+	}
+
+	public String getDate() {
+		return this.date;
+	}
+	
+	public String getHeure() {
+		return this.heure;
+	}
+
+	public String getTemp() {
+		return this.temp;
+	}
+
+}
