49b1cd3e9df5853437d3e9c6d2f72f4598196576
Maël Le Folgoc Pontis <maellefolgocpontis@yahoo.fr>
2019-11-12 09:25:49 +0100

    MMMRRRGGGLLLL

--- thermometre/src/thermometre/outils/OutilsTemperature.java
+++ thermometre/src/thermometre/outils/OutilsTemperature.java
@@ -1,62 +1,95 @@
 package thermometre.outils;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-import thermometre.outils.Temperature;
-
 public class OutilsTemperature {
 	
-	private static ArrayList<Temperature> listeTemp = new ArrayList<Temperature>();
-	
-	private static Scanner entree = new Scanner(System.in);
-	
-	private static void initTemp() {
-		
-	}
-	
-	private static void tempActuelle() {
-		System.out.println(listeTemp.get(listeTemp.size()-1).toString());
-	}
+	public static String convertir(float horaire) {
 
-	private static void tempAnterieure() {
-		System.out.println("Entrer un date et une heure sous la forme jj/mm");
-		String date = entree.nextLine();
-		String heure = entree.nextLine();
-		
-		for (int x = 0 ; x < listeTemp.size() ; x++) {
-			if (date == listeTemp.get(x).getDate() && heure == listeTemp.get(x).getHeure()) {
-				System.out.println(listeTemp.get(x).toString());
-				return;
+		StringBuilder conversion = new StringBuilder(5);                                              
+		float heure,
+		minute,		// minute après conversion en heure et minute
+		seconde;
+
+		if (horaire >= 0 && horaire <= 1439) {
+			heure = horaire / 60;
+			minute = horaire % 60;
+			seconde = horaire - heure - minute;
+			if (heure < 10) {
+				conversion.append("0");
+			}	
+			conversion.append(heure);
+			conversion.append("h");
+			if ( minute < 10) {
+				conversion.append("0");
+			}	
+			conversion.append(minute);
+			conversion.append("m");
+			conversion.append(seconde);
+		}
+		return conversion.toString();
+	}
+	
+	
+	/**
+	 * Convertit l'horaire argument en entier (resultat en minutes)
+	 * Si l'horaire argument n'est pas valide, c'est l'entier -1 qui sera renvoye
+	 * @param horaire  chaîne contenant l'horaire a convertir
+	 * @return un entier egal a la conversion en minutes de l'horaire argument
+	 *                   ou bien a -1 si l'horaire argument n'est pas valide
+	 */
+	public static float convertir(String horaire) {
+		float conversion = -1,
+		heure,
+		minute,
+		seconde;
+
+		if (estValide(horaire)) {
+			heure = horaire.charAt(0) - '0';
+			heure *= 10;
+			heure +=(horaire.charAt(1) - '0');
+			minute = horaire.charAt(3) - '0';
+			minute *= 10;
+			minute += (horaire.charAt(4) - '0');
+			seconde = ((horaire.charAt(6) - '0') + (horaire.charAt(7) - '0'))/100;
+			conversion = heure * 60 + minute + seconde;
+		}
+		return conversion;
+	}
+	
+	/**
+	 * Determine si une chaîne de caractères contient bien un horaire dans le format
+	 * "hh:mm:ss"
+	 * De plus, l'horaire doit etre un horaire de la journee (donc heure compris entre
+	 * 0 et 23 et minute entre 0 et 59)
+	 * @param aTester  chaîne contenant l'horaire a tester
+	 * @return un booleen egal a vrai si l'horaire teste est valide
+	 */
+	public static boolean estValide(String horaire) {
+		boolean resultat = false; // vrai si l'horaire a tester est valide
+		if (horaire.length() == 5) {
+			if(horaire.charAt(2) == ':' && horaire.charAt(5) == ':') {
+				switch (horaire.charAt(0)) {
+					case '1':
+					resultat = horaire.charAt(1) >= '0' && horaire.charAt(1) <= '9' 
+					&& horaire.charAt(3) >= '0' && horaire.charAt(3) <= '5' 
+					&& horaire.charAt(4) >= '0' && horaire.charAt(4) <= '9'
+					&& horaire.charAt(6) >= '0' && horaire.charAt(6) <= '5'
+					&& horaire.charAt(7) >= '0' && horaire.charAt(7) <= '9';
+					break;
+					case '2':
+					resultat = horaire.charAt(1) >= '0' && horaire.charAt(1) <= '3' 
+					&& horaire.charAt(3) >= '0' && horaire.charAt(3) <= '5' 
+					&& horaire.charAt(4) >= '0' && horaire.charAt(4) <= '9'
+					&& horaire.charAt(6) >= '0' && horaire.charAt(6) <= '5'
+					&& horaire.charAt(7) >= '0' && horaire.charAt(7) <= '9';
+					break;
+					default:
+					break;
+				}
 			}
 		}
-		System.out.println("Données innexistantes");
-		
+		return resultat;
 	}
 	
-	private static void diffTemp() {
-		System.out.println("Entrer un date et une heure sous la forme jj/mm");
-		String date = entree.nextLine();
-		String heure = entree.nextLine();
-	}
-
-
-	public static void main(String[] args) {
-		initTemp();
-		int choix = -1;
-		do {
-			entree.hasNextLine();
-			System.out.println("1 : Température actuelle\n2 : Température antérieure\n3 : différence de température\n0 : Quitter");
-			choix = entree.hasNextInt() ? entree.nextInt() : -1;
-			if (choix == 1) {
-				tempActuelle();
-			} else if (choix == 2) {
-				tempAnterieure();
-			} else if (choix == 3) {
-				diffTemp();
-			} else {
-				System.out.println("Entrée incorecte");
-			}
-		} while (choix != 0);
-	}
+	
 
 }
--- thermometre/src/thermometre/outils/Temperature.java
+++ thermometre/src/thermometre/outils/Temperature.java
@@ -1,31 +1,44 @@
 package thermometre.outils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Temperature {
 	
-	private int date;
+	private Date date;
 	
-	private String heure;
+	private int time;
 	
-	private String temp;
+	private float temp;
 		
 	public Temperature(String temp) {
-		this.date = temp.charAt(0)+temp.charAt(1);
+		String ligne= "yyyy/mm/dd hh:mm:ss temp.erature";
+		String[] donnees = ligne.split(" ");
+		donnees[0].split("/");
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
+		try {
+			this.date = formatter.parse(donnees[0] + donnees[1]);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.temp = Float.parseFloat(donnees[2]);
 	}
 
-	public String getDate() {
+	public Date getDate() {
 		return this.date;
 	}
 	
-	public String getHeure() {
-		return this.heure;
+	public int getTime() {	
+		return this.getTime();
 	}
 
-	public String getTemp() {
+	public float getTemp() {
 		return this.temp;
 	}
 	
 	public String toString() {
-		return date + " " + heure + " " + temp;
+		return date + " " + time + " " + temp;
 	}
 }
