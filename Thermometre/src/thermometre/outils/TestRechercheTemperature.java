61559c9deafda84ffe711f271b52a0b8a2b205d9
Maël Le Folgoc Pontis <37334769+Mael-Le-Folgoc-Pontis@users.noreply.github.com>
2019-12-05 12:56:40 +0100

    Update TestRechercheTemperature.java

--- Thermometre/src/thermometre/outils/TestRechercheTemperature.java
+++ Thermometre/src/thermometre/outils/TestRechercheTemperature.java
@@ -7,7 +7,7 @@ import java.util.Date;
 
 import thermometre.outils.RechercheTemperature;
 
-public class testRechercheTemperature {
+public class TestRechercheTemperature {
 	
 	private static ArrayList<Temperature> testTemp = new ArrayList<Temperature>();
 	
@@ -38,12 +38,34 @@ public class testRechercheTemperature {
 			System.out.println("Test avec intervalle négatif OK");
 		}
 	}
+	
+	private static void TestDateOk() {
+		String date1 = "01/11/2019 10:11:12 14.5";
+		String date2 = "02/11/2018 20:11:12 15.5";
+		String date3 = "03/11/2022 20:11:12 16.5";
+		
+		if (RechercheTemperature.dateOk(date1)) {
+			System.out.println("Test avec date correct OK");
+		}
+		if (!RechercheTemperature.dateOk(date2)) {
+			System.out.println("Test avec date trop petite OK");
+		}
+		if (!RechercheTemperature.dateOk(date3)) {
+			System.out.println("Test avec date trop grande OK");
+		}
+	}
+	
+	private static void TestDateIntervalle() {
+		
+	}
 
 	public static void main(String[] args) {
 		RechercheTemperature.editTemp("fichierTempTest.txt");
 		testTemp = RechercheTemperature.getListTemp();
 		// testGetDerniereTemp();
 		// testIntervalleOk();
+		TestDateOk();
+		// TestDateIntervalle();
 	}
 
 }
