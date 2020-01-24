606aeeae8a35042bfc5840a17b506a350847a8a5
Yamkido <pierremarie.combalbert@iut-rodez.fr>
2020-01-24 16:35:50 +0100

    maj interface en plus

---
+++ Thermometre/workspacePTUT/app/src/main/java/projet/src/thermometres3/Supprimer.java
@@ -0,0 +1,14 @@
+package projet.src.thermometres3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Supprimer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supprimer);
+    }
+}
